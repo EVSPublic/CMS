6ddf35250943f4a2c9188a008d9af328cf102097
mohamedelleuch777 <usher7med@gmail.com>
2025-10-24 00:30:30 +0100

    fix build on windows attempt 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,21 +15,10 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       include: ["tailwind.config.js", "node_modules/**"],
-      transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
     },
   },
   optimizeDeps: {
-    include: ["tailwind-config", "react", "react-dom"],
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
+    include: ["tailwind-config"],
   },
   plugins: [react()],
   resolve: {
